434e9434569370d426bcc6d01c8c921fb10f07b7
Davidxdh0 <Davidxdh0@users.noreply.github.com>
2023-06-23 00:14:13 +0200

    done

---
+++ 01/ex05/main.cpp
@@ -0,0 +1,13 @@
+#include "Harl.hpp"
+
+int main()
+{
+	Harl karen;
+
+	karen.complain("debug");
+	karen.complain("info");
+	karen.complain("warning");
+	karen.complain("error");
+	karen.complain("debug");
+	return 0;
+}
