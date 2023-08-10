dce65ce0073b4c3ac3edeb7ccd96f53cada14510
Davidxdh0 <Davidxdh0@users.noreply.github.com>
2023-08-10 18:17:07 +0200

    00

--- 08/ex00/easyfind.hpp
+++ 08/ex00/easyfind.hpp
@@ -1,25 +1,33 @@
 #ifndef Easy_HPP
 #define Easy_HPP
 #include <iostream>
-#include <algorithm>
+#include <algorithm> // find
 #include <deque>
 #include <list>
 #include <set>
 #include <unordered_set>
 #include <vector>
 
-template <typename T> void easyfind(T x, int i){
+template <typename T> 
+int easyfind(T x, int i){
 	try {
-		if (std::find(x.begin(), x.end(), i) != x.end())
-			std::cout << "Found " << i << std::endl;
+		if (x.empty())
+			throw std::runtime_error("Error: Empty container - return fail ");
+		typename T::iterator position = x.begin();
+		position = std::find(x.begin(), x.end(), i);
+		if (position == x.end() || x.empty())
+			throw std::runtime_error("Can't be found in container - return last value: ");
 		else
-			throw std::runtime_error("Can't be found in container");
-	}
-	catch (const std::exception& e) {
-        std::cerr << e.what() << std::endl;
-	}
-	catch(...){
-		std::cout << "Big Error: Can't be found in container" << std::endl;
+			std::cout << "Found i: " << *position << " - return index: " << std::distance(x.begin(), position) << std::endl;
+		return std::distance(x.begin(), position);
+	} catch (const std::exception& e) {
+        
+		if (x.empty()){
+			std::cerr << e.what() << "-1" << std::endl;
+			return (-1);
+		}
+		std::cerr << e.what() << *(std::prev(x.end()))<< std::endl;
+		return (*(std::prev(x.end())));
 	}
 }
 #endif
--- 08/ex00/main.cpp
+++ 08/ex00/main.cpp
@@ -3,42 +3,51 @@
 int main(void) {
 	
     std::vector<int> 		vectorContainer;
-	std::unordered_set<int> unorderedSetContainer;
+	std::vector<int> 		vectorEmpty;
 	std::list<int> 			listContainer;
 	std::deque<int> 		dequeContainer;
 	std::set<int> 			setContainer;
-
+	
 	// vector
     vectorContainer.push_back(1);
-    vectorContainer.push_back(2);
     vectorContainer.push_back(3);
+    vectorContainer.push_back(4);
+	vectorContainer.push_back(7);
+	vectorContainer.push_back(5);
     // list
     listContainer.push_back(1);
-    listContainer.push_back(2);
     listContainer.push_back(3);
+    listContainer.push_back(5);
     // deque
     dequeContainer.push_back(1);
-    dequeContainer.push_back(2);
     dequeContainer.push_back(3);
+    dequeContainer.push_back(5);
     // set
-    setContainer.insert(1);
-    setContainer.insert(2);
     setContainer.insert(3);
-    // unordered_set
-    unorderedSetContainer.insert(1);
-    unorderedSetContainer.insert(2);
-    unorderedSetContainer.insert(3);
+    setContainer.insert(1);
+    setContainer.insert(5);
 
-	easyfind(vectorContainer, 2);
-	easyfind(unorderedSetContainer, 2);
-	easyfind(listContainer, 2);
-	easyfind(dequeContainer, 2);
-	easyfind(setContainer, 2);
 
-	easyfind(vectorContainer, 4);
-	easyfind(unorderedSetContainer, 4);
-	easyfind(listContainer, 4);
-	easyfind(dequeContainer, 4);
-	easyfind(setContainer, 4);
+{
+	auto v = easyfind(vectorContainer, 5);
+	std::cout << v << std::endl;
+	auto b = easyfind(listContainer, 5);
+	std::cout << b << std::endl;
+	auto c = easyfind(dequeContainer, 5);
+	std::cout << c << std::endl;
+	auto d = easyfind(setContainer, 5);
+	std::cout << d << std::endl;	
+}
+	//empty container
+	auto ve = easyfind(vectorEmpty, 8);
+	std::cout << ve << std::endl;
+	auto v = easyfind(vectorContainer, 8);
+	std::cout << v << std::endl;
+	auto b = easyfind(listContainer, 8);
+	std::cout << b << std::endl;
+	auto c = easyfind(dequeContainer, 8);
+	std::cout << c << std::endl;
+	auto d = easyfind(setContainer, 8);
+	std::cout << d << std::endl;	
 	return (1);
 }
